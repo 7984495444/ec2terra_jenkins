fa5925f22e869f138122b4f4ee23114079edb63d
Shah Hiral <73848946+7984495444@users.noreply.github.com>
2022-08-22 18:30:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "test" {
   ami               = "ami-08d4ac5b634553e16"
-  availability_zone = "us-east-1a"
+  #availability_zone = "us-east-1a"
   instance_type     = "t2.micro"
 
   tags = {
